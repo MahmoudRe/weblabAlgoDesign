cc2dace389df64eecec8815cd6f6829695343f91
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2019-12-30 13:37:59 +0100

    greedy - get out the maze

---
+++ src/main/java/GetOutOfMaze.java
@@ -0,0 +1,54 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class GetOutOfMaze {
+
+    public static String outOfMaze(InputStream in) {
+        Scanner sc = new Scanner(in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        Node[] nodes = new Node[n + 1];
+
+        for(int i = 0; i < nodes.length; i++) {
+            nodes[i] =  new Node();
+        }
+
+        while(sc.hasNextInt()) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            sc.nextInt();
+
+            nodes[from].outgoingEdges.add(nodes[to]);
+        }
+
+        sc.close();
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(nodes[start]);
+
+        while(!q.isEmpty()) {
+            Node curr = q.poll();
+
+            if(curr == nodes[end]) return "yes";
+
+            q.addAll(curr.outgoingEdges);
+        }
+
+        return "no";
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        InputStream in = new FileInputStream("src/main/java/getOutOfMaze.txt");
+
+        System.out.println(outOfMaze(in));
+        System.out.println("Actual: yes");
+    }
+}
