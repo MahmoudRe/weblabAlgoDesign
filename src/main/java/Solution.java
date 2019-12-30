207209b7bd6720824ffdf68023ff80cd7911d648
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2019-12-30 21:12:18 +0100

    clean up!

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,79 +1,37 @@
-import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
-
-import java.io.InputStream;
+import java.io.*;
 import java.util.*;
 
 class Solution {
 
-    public String outOfTheMaze(InputStream in) {
+    private static long schedulingCostLogM(InputStream in) {
+
         Scanner sc = new Scanner(in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
-        int end = sc.nextInt();
-
-        Node[] nodes = new Node[n + 1];
-
-        for(int i = 0; i < nodes.length; i++) {
-            nodes[i] =  new Node();
+        int numCases = sc.nextInt();
+        int numCourtRooms = sc.nextInt();
+        long[] caseTimes = new long[numCases];
+        for (int i = 0; i < numCases; i++) {
+            sc.next();
+            caseTimes[i] = sc.nextLong();
         }
-
-        while(sc.hasNextInt()) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            sc.nextInt();
-
-            nodes[from].outgoingEdges.add(nodes[to]);
-        }
-
         sc.close();
 
-        Queue<Node> q = new LinkedList<>();
-        q.add(nodes[start]);
-
-        while(!q.isEmpty()) {
-            Node curr = q.poll();
-
-            if(curr == nodes[end]) return "yes";
-
-            q.addAll(curr.outgoingEdges);
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for (int i = 0; i < numCourtRooms; i++) {
+            pq.add(0l);
         }
-
-        return "no";
+        long result = 0;
+        for (int i = 0; i < numCases; i++) {
+            long earliestCourtRoom = pq.poll();
+            pq.add(earliestCourtRoom + caseTimes[i] + 1);
+            result = Math.max(result, earliestCourtRoom + caseTimes[i]);
+        }
+        return result;
     }
 
-    public String routingTrains(InputStream in) {
-        Scanner sc = new Scanner(in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int s = sc.nextInt();
-        Node[] nodes = new Node[n+1];
+    public static void main(String[] args) throws FileNotFoundException {
 
-        for(int i = 1; i < nodes.length; i++)
-            nodes[i] = new Node();
+        InputStream in = new FileInputStream("src/main/java/getOutOfMaze.txt");
 
-        while(sc.hasNextInt()) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            sc.nextInt();
-
-            nodes[from].outgoingEdges.add(nodes[to]);
-        }
-
-        sc.close();
-
-        Stack<Node> stack = new Stack<>();
-        stack.push(nodes[s]);
-
-        while(!stack.isEmpty()) {
-            Node node = stack.pop();
-
-            if(node.marked) return "yes";
-            node.marked = true;
-
-            stack.addAll(node.outgoingEdges);
-        }
-
-        return "no";
+        System.out.println(schedulingCostLogM(in));
     }
 }
