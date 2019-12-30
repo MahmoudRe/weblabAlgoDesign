d04dad8a8d8c90746b8217b69ba3ff3703be95d6
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2019-12-30 14:00:29 +0100

    greedy - get out of the maze fastest

--- src/main/java/GetOutOfMaze.java
+++ src/main/java/GetOutOfMaze.java
@@ -42,6 +42,62 @@ public class GetOutOfMaze {
         return "no";
     }
 
+    public static String outOfTheMazeFastest(InputStream in) {
+        Scanner sc = new Scanner(in);
+        /*
+         * We already parse the input for you and should not need to make changes to this part of the code.
+         * You are free to change this input format however.
+         */
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int s = sc.nextInt();
+        int t = sc.nextInt();
+        ArrayList<HashMap<Integer, Integer>> nodes = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            nodes.add(new HashMap<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int cost = sc.nextInt();
+            nodes.get(u).put(v, cost);
+        }
+
+        sc.close();
+
+        PriorityQueue<MazeNode2> q = new PriorityQueue<>();
+        q.add(new MazeNode2(s, nodes.get(s).size()));
+
+        int[] costs = new int[n+1];     //the index is node number, the value is the cost
+        Arrays.fill(costs, Integer.MAX_VALUE);  //at the beginning all node-costs are infinite
+        boolean[] visited = new boolean[n+1];
+        costs[s] = nodes.get(s).size(); //the cost of the start node is just #outGoingEdges
+
+        while(!q.isEmpty()) {
+            MazeNode2 curr = q.poll();
+            visited[curr.i] = true;
+            if (curr.i == t) break;
+
+            for(int child : nodes.get(curr.i).keySet()) {
+                // cost = #outGoingEdges + distance(start -> currNode) + cost (currNode -> childNode)
+                int cost = nodes.get(child).size() + costs[curr.i] + nodes.get(curr.i).get(child);
+
+                //in case the child is the endNode, remove #outGoingEdgjes from the cost as this doesn't count.
+                if (child == t) costs[child] -= nodes.get(child).size();
+
+                if(cost < costs[child]) {
+                    q.add(new MazeNode2(child, cost));
+                    costs[child] = cost;
+                }
+            }
+        }
+
+        if(costs[t] == Integer.MAX_VALUE) return "-1";
+        return Integer.toString(costs[t]);
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
 
         InputStream in = new FileInputStream("src/main/java/getOutOfMaze.txt");
@@ -59,5 +115,19 @@ class MazeNode {
     public MazeNode() {
         this.outgoingEdges = new ArrayList<>();
         this.marked = false;
+    }
+}
+
+class MazeNode2 implements Comparable<MazeNode2> {
+    int i, cost;
+
+    public MazeNode2(int i, int cost) {
+        this.i = i;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(MazeNode2 n2) {
+        return this.cost - n2.cost;
     }
 }
